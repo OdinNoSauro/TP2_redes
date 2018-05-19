4f22faf89844c0c928d2fffaebbe8474b2c87e02
Ariel <dsrariel@gmail.com>
2018-05-19 19:37:13 -0300

    Comparação das somas de verificação OK

--- cliente.c
+++ cliente.c
@@ -1,22 +1,31 @@
 // Cliente
 
-	#include <stdlib.h>
-	#include <stdio.h>
-	#include <unistd.h>
-	#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
 
-	#include <sys/time.h>
-	#include <netinet/in.h>
-	#include <arpa/inet.h>
+#include <string.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-	#include "tp_socket.h"
+#include "tp_socket.h"
 
-	#define AMOSTRAS 1
+#define AMOSTRAS 1
+#define TAMANHO_CABECALHO 31
 
-	int main (int argc, char *argv[]){
+void intParaChar(int inteiro, char* vetor, int inicio, int termino);
+int charParaInt(const char* vetor, int inicio, int termino);
+int somaDeVerificacao(const char* buffer);
+int enviaPacote(int somaDeVerificacao, int tamanhoDados, int numero_de_sequencia, int ACK, int flag, char* buffer, int socket_des, so_addr* destino);
+int comparaSomas(const char* buffer);
+
+int main (int argc, char *argv[]){
 	tp_init();
 
 	char HOST_SERVIDOR[16];
+	memset(HOST_SERVIDOR, 0x0, 16);
 	strcpy(HOST_SERVIDOR, argv[1]);//host_do_servidor
 	int PORTA_SERVIDOR = atoi(argv[2]); // porta da conexão
 	char NOME_ARQUIVO[20];
@@ -27,7 +36,7 @@
 	struct timeval inicio, fim;
 	so_addr servidor;
 
-	int PORTA_CLIENTE = PORTA_SERVIDOR;
+	int PORTA_CLIENTE = 2000;
 	int PORTA_SERVIDOR_ORIGINAL = PORTA_SERVIDOR;
 	int mensagens = 0;
 	int vez;
@@ -47,6 +56,8 @@
 		int socket_des; // descritor do socket
 		int bytes_recebidos = 0;
 		int x = 0;
+		int numero_de_sequencia;
+		int ACK = 0;
 
 		settimeofday(NULL, NULL);
 		gettimeofday(&inicio, NULL);
@@ -75,6 +86,7 @@
 
 
 		//Recebe arquivo
+		/*========ORIGINAL========
 		FILE *fp = fopen((const char*) NOME_ARQUIVO, "w+");
 		memset(buffer, 0x0, LENGTH);
 		do{
@@ -91,12 +103,30 @@
 			}
 			printf("%s\n",buffer);
 			memset(buffer, 0x0, x);
-		}while();
-		printf("Conexão encerrada \n");
+		}while();*/
 
+		//1º Recebe pacote do servidor
+		memset(buffer, 0x0, LENGTH);
+		tp_recvfrom(socket_des, buffer, LENGTH, &servidor);
+		x = strlen(buffer);
+		bytes_recebidos += x;
+		mensagens++;
+
+		//2º Faz soma de verificação e, se correta, verifica número de sequência
+		if(comparaSomas(buffer)){
+			printf("SOMAS IGUAIS!\n");
+			printf("BUFFER: %s\n", buffer);
+		}
+		else{
+			printf("SOMAS DIFERENTES!");
+		}
+		//3º Se número de sequência igual a ACK, insere no arquivo e envia ACK
+		//4º Se não, reenvia último pacote
+		//4º Faz isso até receber flag = 1 no passo 3
 
 		//Encerra e limpa a memória
-		fclose(fp);
+		printf("Conexão encerrada \n");
+		//fclose(fp);
 		close(socket_des);
 		free(buffer);
 
@@ -122,4 +152,123 @@
 	printf("Desvio padrão do tempo: %fs\n", desvio);
 	printf("Vazão média: %10.2fkbps\n", media_v/AMOSTRAS);
 	return 0;
+}
+
+void intParaChar(int inteiro, char* vetor, int inicio, int termino){
+    for(int i = termino; i >= inicio; i--){
+    int aux = inteiro%10;
+    inteiro = inteiro/10;
+        switch (aux){
+            case 0:
+                vetor[i] = '0';
+                break;
+            case 1:
+                vetor[i] = '1';
+                break;
+            case 2:
+                vetor[i] = '2';
+                break;
+            case 3:
+                vetor[i] = '3';
+                break;
+            case 4:
+                vetor[i] = '4';
+                break;
+            case 5:
+                vetor[i] = '5';
+                break;
+            case 6:
+                vetor[i] = '6';
+                break;
+            case 7:
+                vetor[i] = '7';
+                break;
+            case 8:
+                vetor[i] = '8';
+                break;
+            case 9:
+                vetor[i] = '9';
+                break;
+        }
+    }
+}
+
+int charParaInt(const char* vetor, int inicio, int termino){
+	int retorno = 0;
+	for (int i=inicio; i <= termino; i++){
+        retorno *= 10;
+		switch (vetor[i]){
+            case '1':
+                retorno += 1;
+                break;
+            case '2':
+                retorno += 2;
+                break;
+            case '3':
+                retorno += 3;
+                break;
+            case '4':
+                retorno += 4;
+                break;
+            case '5':
+                retorno += 5;
+                break;
+            case '6':
+                retorno += 6;
+                break;
+            case '7':
+                retorno += 7;
+                break;
+            case '8':
+                retorno += 8;
+                break;
+            case '9':
+                retorno += 9;
+                break;
+        }
+	}
+	return retorno;
+}
+
+int somaDeVerificacao(const char* buffer){
+    int soma_buffer = 0;
+
+    for (int i = TAMANHO_CABECALHO; i < strlen(buffer); i++){
+        soma_buffer += (int) buffer[i];
+        
+        //131071(base 10) = 1111.1111.1111.1111(base 2)
+        if (soma_buffer > 131071) soma_buffer -= 131071;
+    
+    }
+    
+    return soma_buffer;
+}
+
+int enviaPacote(int somaDeVerificacao, int tamanhoDados, int numero_de_sequencia, int ACK, int flag, char* buffer, int socket_des, so_addr* destino){
+	char cabecalho[TAMANHO_CABECALHO];
+	/*	cabecalho[0-5] -> Soma de verificação;
+	  	cabecalho[6-9] -> Tamanho dados;
+		cabecalho[10-19] -> Número de sequência;
+		cabecalho[20-29] -> ACK;
+		cabecalho[30] -> flag.
+	*/
+	intParaChar(somaDeVerificacao, cabecalho, 0, 5);
+	intParaChar(tamanhoDados, cabecalho, 6, 9);
+	intParaChar(numero_de_sequencia, cabecalho, 10, 19);
+	intParaChar(ACK, cabecalho, 20, 29);
+	intParaChar(flag, cabecalho, 30, 30);
+	
+	memcpy(buffer, cabecalho, TAMANHO_CABECALHO);
+
+	return tp_sendto(socket_des, buffer, strlen(buffer), destino);
+}
+
+int comparaSomas(const char* buffer){
+	int soma_verificacao_local, soma_verificacao_pacote;
+	
+	soma_verificacao_local = somaDeVerificacao(buffer);
+	soma_verificacao_pacote = charParaInt(buffer, 0, 5);
+
+	if (soma_verificacao_local == soma_verificacao_pacote) return 1;
+	else return 0;
 }
